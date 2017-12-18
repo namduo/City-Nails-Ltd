b1df4e41d13bcdd1f53ec9a3efcde33fde4b00ad
Nam Nguyen <nm.duong@outlook.com>
2017-12-18 21:49:19 +0000

    first commit

---
+++ public/javascripts/main.js
@@ -0,0 +1,36 @@
+
+// FORM SUBMISSION
+document.getElementById('contact-form').addEventListener('submit', formSubmission);
+
+function formSubmission() {
+	event.preventDefault();
+
+	// FORM VALIDATE
+	let formName = document.getElementById('name-field').value;
+	let formEmail = document.getElementById('email-field').value;
+	let formMessage = document.getElementById('message-field').value;
+
+	if (formName === '' || formEmail === '' || formMessage === '') {
+		alert('Please Fill In All Fields');
+	} else {
+		console.log('form submitted');
+
+		fetch('/contact/form', {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json, text/plain, */*', 'Content-type':'application/json'
+			},
+			body:JSON.stringify({
+				name: formName,
+				email: formEmail,
+				message: formMessage
+			})
+		})
+		.then((res) => res.json())
+		.then((data) => console.log(data))
+		.catch((err) => console.log(err))
+
+		document.getElementById('contact-form').reset();
+	}
+
+}
---
+++ routes/contact.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var key = require('../config/keys')
+
+var mailgun = require('mailgun-js')({apiKey: key.mailgun.api_key, domain: key.mailgun.domain_key});
+
+/* GET contact page. */
+router.get('/', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+/* POST contact form. */
+router.post('/form', function(req, res) {
+  var data = {
+    from: 'Mailgun - Portfolio <postmaster@sandboxc23c218c34754889bcd25fb2e7baba2f.mailgun.org>',
+    to: 'nm.duong@outlook.com',
+    subject: 'Message from ' + req.body.name + ': ' + req.body.email,
+    text: req.body.message
+  };
+
+  mailgun.messages().send(data, function (error, body) {
+    console.log(body);
+    if(!error)
+			res.json({});
+    else
+      res.send('Mail not sent');
+  });
+
+});
+
+
+
+module.exports = router;
