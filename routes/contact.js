0d58cffc779555259f278a20f788894343c75edd
Nam Nguyen <nm.duong@outlook.com>
2019-05-15 09:44:31 +0100

    Email JS update

--- routes/contact.js
+++ routes/contact.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 router.post('/', function(req, res) {
    
    axios.post('https://api.emailjs.com/api/v1.0/email/send', {
-      service_id: 'citynails_gmail',
+      service_id: 'citynails_gmail_19',
       template_id: 'template__2',
       user_id: 'user_eE3zF4XZczCQxqFROwutS',
       template_params: {
